b9e6de25a8a58fdb542618686f46ea3f6c5f034a
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-06-06 22:45:53 +0530

    Update 1.Quicksort.cpp

--- 1.Quicksort.cpp
+++ 1.Quicksort.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<ctime>
 #include<fstream>
-
 using namespace std;
 
 int partition(int a[],int l,int h){
     int pivot=a[l];
-    int i=l-1, j=h,temp;
+    int i=l,j=h,temp;
     
     while(i<j){
         do{
@@ -15,7 +14,7 @@ int partition(int a[],int l,int h){
         do{
             j--;
         }while(a[j]>pivot);
-        if(h<l){
+        if(i<j){
            temp=a[i];
            a[i]=a[j];
            a[j]=temp;
@@ -26,19 +25,17 @@ int partition(int a[],int l,int h){
     a[j]=temp;
     return j;
 }
-
 void quicksort(int a[],int l,int h){
     if(l<h){
         int j=partition(a,l,h);
-        quicksort(a,0,j-1);
+        quicksort(a,l,j);
         quicksort(a,j+1,h);
     }
 }
 
 int main(){
 int n, a[1000],i,j;
-
-cout<<"Enter the No. of Elements :";
+cout<<"Enter the Number of Elements to be sorted :";
 cin>>n;
 cout<<"The Random numbers are :"; 
 for(i=0;i<n;i++){
@@ -54,13 +51,13 @@ cout<<"\nSorted Elements are :";
 for(i=0;i<n;i++)
    cout<<a[i]<<" ";
 double time=(double) (end-start)/CLOCKS_PER_SEC;
-cout<<"\nTime taken is :"<<time;
+cout<<"\nTime taken for sorted "<<n<<" elements is :"<<time; 
 
-for(i=100;i<=1000;i+50){
+for(i=10000;i<=100000;i+=2000){
         n=i;
         int a[n];
         for(j=0;j<n;j++)
-           a[j]=rand()%1000;
+           a[j]=rand()%100;
         clock_t start=clock();
         quicksort(a,0,n);
         clock_t end=clock();
@@ -70,4 +67,6 @@ for(i=100;i<=1000;i+50){
         file<<n<<"\t"<<t<<"\n";
         file.close();
     }
+cout<<"\nTime to sort 10000 to 100000 (incrementing 2000 per iteration) element is added to the file quicksort.txt ";
+return 0;
 }
