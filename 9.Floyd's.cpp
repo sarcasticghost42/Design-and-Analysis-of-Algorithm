630a898ee77d9aaad4e60e15a430383da90bc218
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-04 21:45:51 +0530

    Create 9. Floyd's.cpp

---
+++ 9.Floyd's.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+#define INF 999
+#define min(x,y) (((x)>(y) ? y: x))
+
+void createGraph(int adj[MAX][MAX], int n)
+{
+    printf("Enter Adj Matrix: (999 for inf)\n ");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+}
+
+void floyd(int adj[MAX][MAX], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                adj[j][k] = min(adj[j][k], adj[j][i] + adj[i][k]);
+            }
+        }
+    }
+}
+
+void printGraph(int adj[MAX][MAX], int n)
+{
+    printf("After:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter No of Vertices: ");
+    scanf("%d", &n);
+
+    int adj[MAX][MAX];
+
+    createGraph(adj, n);
+    floyd(adj, n);
+    printGraph(adj, n);
+
+    return 0;
+}
